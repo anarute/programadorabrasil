3d33e503764dd5b422966d8ced142dc0f7adb0a0
buruno <hi@buruno.com.br>
2014-10-17 18:05:32 -0300

    partial angular integration (missed some files)

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -1,4 +1,4 @@
-angular.module( 'ngBoilerplate.about', [
+angular.module( 'programadoraBrasil.about', [
   'ui.router',
   'placeholders',
   'ui.bootstrap'
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
-angular.module( 'ngBoilerplate', [
+angular.module( 'programadoraBrasil', [
   'templates-app',
   'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
+  'programadoraBrasil.home',
+  'programadoraBrasil.about',
   'ui.router'
 ])
 
